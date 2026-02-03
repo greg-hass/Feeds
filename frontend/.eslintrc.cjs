7e0a1b578ab8c51aa46c8e73ddde1b4d80b64051
Greg <galcorn@icloud.com>
2026-02-03 18:33:19 +0000

    fix(eslint): add Detox globals and ignore test directories
    Add globals for E2E test functions (describe, it, device, etc.)
    and ignore e2e/ and __tests__/ directories to avoid false positives
    from lint checks that don't apply to test code.

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -3,5 +3,21 @@ module.exports = {
     extends: ['expo'],
     rules: {
         '@typescript-eslint/ban-types': 'off',
+        'no-undef': 'off',
     },
+    globals: {
+        describe: 'readonly',
+        it: 'readonly',
+        test: 'readonly',
+        expect: 'readonly',
+        beforeAll: 'readonly',
+        beforeEach: 'readonly',
+        afterAll: 'readonly',
+        afterEach: 'readonly',
+        device: 'readonly',
+        element: 'readonly',
+        by: 'readonly',
+        waitFor: 'readonly',
+    },
+    ignorePatterns: ['e2e/', '__tests__/'],
 };
